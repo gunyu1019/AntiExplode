bb81fa3658945d73a0ba56319594933126bb25e1
gunyu1019 <gunyu1019@gmail.com>
2022-05-23 17:54:45 +0900

    change gradle

---
+++ build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    kotlin("jvm") version "1.6.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+
+    `maven-publish`
+}
+
+group = "kr.yhs"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+    maven("https://papermc.io/repo/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly(kotlin("stdlib"))
+}
+
+tasks {
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    processResources {
+        filesMatching("**/*.yml") {
+            expand(project.properties)
+        }
+        filteringCharset = "UTF-8"
+    }
+    shadowJar {
+        archiveBaseName.set(project.name)
+        archiveVersion.set("")
+        archiveClassifier.set("")
+    }
+    create<Copy>("dist") {
+        from (shadowJar)
+        into(".\\")
+    }
+}
